c78cb72c43976ffc979f92d5a21662d649975a45
Mayuri Rathod <mayuri.gr@gmail.com>
2019-02-22 13:52:44 +0800

    added from

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 
-export const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+export default class App extends React.Component {
+  onFormsubmit = term => {
+    console.log(term);
+  };
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onFormsubmit} />
+      </div>
+    );
+  }
+}
