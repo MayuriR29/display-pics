c8b4c0043762e9e60eb79f9d86b02dd0fd4fe6a8
Mayuri Rathod <mayuri.gr@gmail.com>
2019-02-22 14:41:53 +0800

    added axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 export default class App extends React.Component {
-  onFormsubmit = term => {
-    console.log(term);
-  };
+  async onSearchsubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 27dcaaec595f2e63853490d502aa701a0ec2ebc6412cb026706de1a35e0c0a75"
+      }
+    });
+    console.log(response.data.results);
+  }
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar onSubmit={this.onFormsubmit} />
+        <SearchBar onSubmit={this.onSearchsubmit} />
       </div>
     );
   }
