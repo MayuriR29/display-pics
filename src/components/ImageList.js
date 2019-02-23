d0d6e91431be4c9816088cc72d59035c29c78879
Mayuri Rathod <mayuri.gr@gmail.com>
2019-02-23 13:59:42 +0800

    destructing elements

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,7 @@
 import React from "react";
 const ImageList = props => {
-  const images = props.images.map((eachImage, id) => {
-    return <img alt="" src={eachImage.urls.regular} key={eachImage.id} />;
+  const images = props.images.map(({ description, urls, id }) => {
+    return <img alt={description} src={urls.regular} key={id} />;
   });
   return <div>{images}</div>;
 };
