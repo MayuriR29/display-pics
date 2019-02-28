e1c029bf078b4fb4f6e5ddcb20463a656572a783
Mayuri Rathod <mayuri.gr@gmail.com>
2019-02-28 12:33:38 +0800

    created imageCard component

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,15 +1,10 @@
 import React from "react";
 import "./ImageList.css";
+import ImageCard from "./ImageCard";
+
 const ImageList = props => {
-  const images = props.images.map(({ description, urls, id }) => {
-    return (
-      <img
-        className="image-list"
-        alt={description}
-        src={urls.regular}
-        key={id}
-      />
-    );
+  const images = props.images.map(eachImage => {
+    return <ImageCard key={eachImage.id} image={eachImage} />;
   });
   return <div className="image-list">{images}</div>;
 };
